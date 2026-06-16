03b384dbec14b8a787990f4ea918fda2c4e22c26
Martin Bergeron <marxtinus@MacBookAir-de-Martin.local>
2026-06-16 18:49:40 -0400

    initial project

---
+++ resources/js/pages/carrousel.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'carrousel',
+});
+</script>
+
+<template>
+    Hello World de carrousel
+</template>
+
+<style scoped></style>
