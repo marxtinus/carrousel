ac260bd672367bcb59ac5e6ef3648504cfd9fcca
marxtinus <marxtinus@gmail.com>
2026-06-16 19:19:54 -0400

    carrousel component with

--- resources/js/pages/carrousel.vue
+++ resources/js/pages/carrousel.vue
@@ -1,13 +1,24 @@
-<script lang="ts">
-import { defineComponent } from 'vue';
+<script setup lang="ts">
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel';
+import 'vue3-carousel/dist/carousel.css';
 
-export default defineComponent({
-    name: 'carrousel',
-});
+const carouselConfig = {
+    itemsToShow: 2.5,
+    wrapAround: true,
+};
 </script>
 
 <template>
-    Hello World de carrousel
+    <Carousel v-bind="carouselConfig">
+        <Slide v-for="slide in 10" :key="slide">
+            <div class="carousel__item">{{ slide }}</div>
+        </Slide>
+
+        <template #addons>
+            <Navigation />
+            <Pagination />
+        </template>
+    </Carousel>
 </template>
 
 <style scoped></style>
