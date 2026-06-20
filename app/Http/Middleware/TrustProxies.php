79be51b6d830ef12d0fb288361f527ffeab2646e
marxtinus <marxtinus@gmail.com>
2026-06-20 17:25:13 -0400

    trust proxy

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+use Illuminate\Http\Request;
+
+class TrustProxies extends Middleware
+{
+    /**
+     * Les proxies de confiance
+     */
+    protected $proxies = '*';
+
+    /**
+     * Les headers utilisés
+     */
+    protected $headers =
+        Request::HEADER_X_FORWARDED_FOR |
+        Request::HEADER_X_FORWARDED_HOST |
+        Request::HEADER_X_FORWARDED_PORT |
+        Request::HEADER_X_FORWARDED_PROTO;
+}
